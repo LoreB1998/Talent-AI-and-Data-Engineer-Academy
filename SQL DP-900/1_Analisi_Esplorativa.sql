c7edec22bf3a494e84016c58d1e7a81e3d033636
Lorenzo Barbato <lorenzobarbato1998@gmail.com>
2026-05-28 12:21:53 +0200

    Aggiunta analisi esplorativa e pulizia delle tabelle nel script SQL

--- SQL DP-900/1_Analisi_Esplorativa.sql
+++ SQL DP-900/1_Analisi_Esplorativa.sql
@@ -1,3 +1,4 @@
+-- Analisi esplorativa
 SELECT TOP 10 *
 FROM SalesLT.Address;
 
@@ -27,3 +28,45 @@ FROM SalesLT.SalesOrderDetail;
 
 SELECT TOP 10 *
 FROM SalesLT.SalesOrderHeader;
+
+
+-- Verifica e pulizia delle tabelle
+SELECT
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    COLUMN_NAME,
+    DATA_TYPE,
+    CHARACTER_MAXIMUM_LENGTH AS max_lunghezza,
+    IS_NULLABLE AS accetta_null
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'SalesLT'
+ORDER BY TABLE_NAME, ORDINAL_POSITION;
+
+
+
+-- Colori con media StandardCost > 550
+WITH color_cost AS (
+    SELECT  Color, AVG(StandardCost) OVER (PARTITION BY Color) AS avg_by_color
+    FROM SalesLT.Product
+    WHERE Color IS NOT NULL)
+SELECT DISTINCT Color
+FROM color_cost
+WHERE avg_by_color > 550;
+
+-- Percentuale di colori
+WITH color_cnt_pct AS (
+    SELECT Color,
+           COUNT(ProductID) AS cnt,
+           SUM(COUNT(ProductID)) OVER () AS tot
+    FROM SalesLT.Product
+    WHERE Color IS NOT NULL
+    GROUP BY Color)
+SELECT Color,
+       CONCAT(CAST((cnt * 100.0 / tot) AS DECIMAL(5, 2)), '%') AS pct
+FROM color_cnt_pct;
+
+
+
+
+
+
