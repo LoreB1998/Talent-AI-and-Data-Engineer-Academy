5967138ce4837dbbf4160f92bebfe2714f472468
Lorenzo Barbato <lorenzobarbato1998@gmail.com>
2026-05-28 10:39:05 +0200

    Aggiunto script SQL per analisi esplorativa dei dati

---
+++ SQL DP-900/1_Analisi_Esplorativa.sql
@@ -0,0 +1,29 @@
+SELECT TOP 10 *
+FROM SalesLT.Address;
+
+SELECT TOP 10 *
+FROM SalesLT.Customer;
+
+SELECT TOP 10 *
+FROM SalesLT.CustomerAddress;
+
+SELECT TOP 10 *
+FROM SalesLT.Product;
+
+SELECT TOP 10 *
+FROM SalesLT.ProductCategory;
+
+SELECT TOP 10 *
+FROM SalesLT.ProductDescription;
+
+SELECT TOP 10 *
+FROM SalesLT.ProductModel;
+
+SELECT TOP 10 *
+FROM SalesLT.ProductModelProductDescription;
+
+SELECT TOP 10 *
+FROM SalesLT.SalesOrderDetail;
+
+SELECT TOP 10 *
+FROM SalesLT.SalesOrderHeader;
